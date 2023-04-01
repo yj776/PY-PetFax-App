f60e879454bb007d27270f01856ac505797d260b
Amii Jiang <amii@192.168.0.107>
2023-04-02 02:32:11 +0800

    updates

--- petfax/fact.py
+++ petfax/fact.py
@@ -1,7 +1,27 @@
-from flask import ( Blueprint, render_template ) 
+from flask import ( Blueprint, render_template, request, redirect ) 
+from . import models
 
 bp = Blueprint('fact', __name__, url_prefix="/facts")
+
+@bp.route('/', methods=['GET', 'POST']) 
+def index(): 
+    if request.method == 'POST':
+        submitter = request.form['submitter']
+        fact = request.form['fact']
+        
+        new_fact = models.Fact(submitter=submitter, fact=fact)
+        models.db.session.add(new_fact)
+        models.db.session.commit()
+        return redirect('/facts')
+    
+    return render_template('facts/index.html')
 
 @bp.route('/new')
 def new(): 
     return render_template('facts/new.html')
+
+
+
+
+
+
